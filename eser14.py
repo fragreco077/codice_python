33f19a4a67b42aca714425771d92d939ac5e6a35
francesco greco <f.greco077@gmail.com>
2024-11-14 10:27:15 -0500

    up

---
+++ eser14.py
@@ -0,0 +1,14 @@
+latoq= float (input ("quanto misura il lato?\n"))
+periq= latoq * 4
+print ("il perimetro è ",(periq))
+
+
+base = float (input("quanto misura la base \n"))
+alt = float(input("quanto misura l'altezza\n"))
+perir = base*2 + alt*2 
+print("il perimetro è ",(perir))
+
+rc= float(input("quanto misura il raggio "))
+pi = 3.14159
+circon =  2*pi*rc
+print("la circonferenza è ",(circon))
